04e9c656d2897e7341f95d812464ccf06efea845
Ben Parkinson <benjamesparkinson@gmail.com>
2020-12-05 16:09:47 +0100

    Added new route for sending manual moves to other player

--- src/main/java/com/parkinson/ben/gameofthree/component/controller/ManualInputController.java
+++ src/main/java/com/parkinson/ben/gameofthree/component/controller/ManualInputController.java
@@ -4,10 +4,11 @@ import com.parkinson.ben.gameofthree.model.GameMove;
 import com.parkinson.ben.gameofthree.service.IGameService;
 import com.parkinson.ben.gameofthree.service.IOtherPlayerService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/gameofthree")
@@ -23,9 +24,16 @@ public class ManualInputController {
     }
 
     @PostMapping("/v1/api/games")
+    @ResponseStatus(HttpStatus.CREATED)
     public void startNewGame() {
         GameMove firstMove = gameService.startGameWithRandomMove();
 
         otherPlayerService.sendNextMove(firstMove);
+    }
+
+    @PostMapping("/v1/api/manual/gamemoves")
+    @ResponseStatus(HttpStatus.CREATED)
+    public void forwardManualGameMove(@Valid @RequestBody GameMove gameMove) {
+        otherPlayerService.sendNextMove(gameMove);
     }
 }
---
+++ src/test/java/com/parkinson/ben/gameofthree/testing/contract/ManualInputControllerIT.java
@@ -0,0 +1,52 @@
+package com.parkinson.ben.gameofthree.testing.contract;
+
+import com.parkinson.ben.gameofthree.model.GameMove;
+import io.restassured.RestAssured;
+import org.apache.http.HttpStatus;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.net.URI;
+
+// note that for now this depends on both player servers running
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {
+        TestContext.class
+})
+public class ManualInputControllerIT {
+
+    @Autowired
+    private URI serverUri;
+
+    @Test
+    public void testManualValidMoveIsAccepted() {
+        GameMove gameMove = new GameMove(18, 0);
+        RestAssured.with()
+                .header("Content-Type", "application/json")
+                .body(gameMove)
+                .when().post(serverUri + "/gameofthree/v1/api/manual/gamemoves")
+                .then().statusCode(HttpStatus.SC_CREATED);
+    }
+
+    @Test
+    public void testInvalidManualMoveRespondsWithBadRequest() {
+        GameMove gameMove = new GameMove(18, 5);
+        RestAssured.with()
+                .header("Content-Type", "application/json")
+                .body(gameMove)
+                .when().post(serverUri + "/gameofthree/v1/api/manual/gamemoves")
+                .then().statusCode(HttpStatus.SC_BAD_REQUEST);
+    }
+
+    @Test
+    public void testStartGameRespondsWithCreated() {
+        RestAssured.with()
+                .header("Content-Type", "application/json")
+                .when().post(serverUri + "/gameofthree/v1/api/games")
+                .then().statusCode(HttpStatus.SC_CREATED);
+    }
+
+}
