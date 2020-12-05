35973602cbb80087a9d80a7f02ed878818793a62
Ben Parkinson <benjamesparkinson@gmail.com>
2020-12-05 21:58:24 +0100

    Switched to use webjars for web dependencies. Added subscriptions to two websocket topics, only log to console for now.

--- src/main/resources/static/js/gameofthree.js
+++ src/main/resources/static/js/gameofthree.js
@@ -1,14 +1,38 @@
+var stompClient = null;
+
+function parseMoveMessage(message) {
+    return JSON.parse(message.body);
+}
+
+function connectToWebSocket() {
+    var socket = new SockJS('/game-of-three-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.debug = () => { };
+    stompClient.connect({}, function () {
+        stompClient.subscribe('/topic/my-moves', function (myMove) {
+            var move = parseMoveMessage(myMove);
+            console.log('mymove: ' + move.result + ', ' + move.addend);
+        });
+        stompClient.subscribe('/topic/their-moves', function (theirMove) {
+            var move = parseMoveMessage(theirMove);
+            console.log('theirmove: ' + move.result + ', ' + move.addend);
+        });
+    });
+}
+
 $(document).ready(function () {
     $('#startButton').on('click', function () {
         $.ajax({
             type: 'POST',
             url: '/gameofthree/v1/api/games',
-            success: function(firstMove) {
+            success: function (firstMove) {
                 $('#feedback-message').text('My first move: ' + firstMove.result);
             },
-            error: function() {
+            error: function () {
                 $('#feedback-message').text('Error starting game');
             }
         })
     });
+
+    connectToWebSocket();
 });
